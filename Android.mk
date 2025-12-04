9e3fd23f2622228ec349062855204e3e4e972fb3
hasan6034 <h.basri3460@gmail.com>
2025-12-04 14:00:51 +0000

    ishtar: Initial vendor tree
    from OS2.0.210.0VMACNXM
    
    Signed-off-by: hasan6034 <h.basri3460@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),ishtar)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b067d09804a4b4370ef223764b19f2f61705ed79)
+$(call add-radio-file-sha1-checked,radio/aop.img,ffd40d5e57ed616ac82ffb369d55a4da78c198e8)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,56ef77d25dc73e33a6a6230f40eb0ad826b15551)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,d22eb2f3209b193640d731e9ac12ab77271c7dc9)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,a97c1b3c6cc17f21a249e1d153c5f92f43fcce90)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,6cdfcb7616c7b83c9a7d016d72149628a94fb11c)
+$(call add-radio-file-sha1-checked,radio/dsp.img,6d1dd5292b98159de65149f0f8519cc46ed8d854)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,ded4f76f623b60584f933447949adc450ea40dea)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e1bfe67d59e11ac16ae8543695f1dc6a0e7c754f)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,b3a5eb011ca19c43fd84a92a8a8b10a5315d2f82)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,70b67b8ad17204bfb1146f466fb22fe0911e3d25)
+$(call add-radio-file-sha1-checked,radio/modem.img,4f9e4fe1f951ca2b87c7db7f768b5192066072ea)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,accfcf72cb6f6dd7fb9a9cf813e906c448b50696)
+$(call add-radio-file-sha1-checked,radio/shrm.img,68935c5f9488a35e9c407e2eec41d4f90421f95f)
+$(call add-radio-file-sha1-checked,radio/tz.img,9210bc15959debc32fcecce00bee51368cb3c91b)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ac53f9dd608f29e94bbc407bb3beb274f2da9357)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,a060616a02c3c0910e7b00059c345a0d29a66ba2)
+$(call add-radio-file-sha1-checked,radio/xbl.img,79fb16e3d358ef5b523bc8712df0cafc28793771)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,756e533d2e87c6c2ce4c779ad231ade72294660a)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,761cd726c9ced42b20d88d930e0a99602c00eb6a)
+
+endif
